44521274708125c5122acf25fe0ad3fceea18073
Noe-Sanchez <danfoot4567@gmai.com>
2025-01-31 12:36:34 +0100

    Rudimentary nav

--- src/nav_test.cpp
+++ src/nav_test.cpp
@@ -3,11 +3,13 @@
 #include <memory>
 #include <string>
 #include <iostream>
+#include <unistd.h>
 
 #include "nav_msgs/msg/odometry.hpp"
 #include "icuas25_msgs/msg/target_info.hpp"
 #include "ros2_aruco_interfaces/msg/aruco_markers.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "crazyflie_interfaces/srv/go_to.hpp"
 
 #include "rclcpp/rclcpp.hpp"
 
@@ -24,7 +26,7 @@ class NavTest : public rclcpp::Node{
       target_publisher = this->create_publisher<icuas25_msgs::msg::TargetInfo>("target_found", 10);
 
       // Service client
-      goto_client = this->create_client<crazyflie_interfaces::srv::Goto>("cf_1/go_to");
+      goto_client = this->create_client<crazyflie_interfaces::srv::GoTo>("cf_1/go_to");
 
       // Make 0.1s timer
       //control_timer = this->create_wall_timer(10ms, std::bind(&NavTest::control_callback, this));
@@ -32,13 +34,22 @@ class NavTest : public rclcpp::Node{
       // Initial goto instruction
 
       // Call the service
-      request = std::make_shared<crazyflie_interfaces::srv::Goto::Request>();
-      request->goal.x = 0.0;
-      request->goal.y = 1.0;
+      request = std::make_shared<crazyflie_interfaces::srv::GoTo::Request>();
+      request->goal.x = 1.0;
+      request->goal.y = 0.0;
       request->goal.z = 1.0;
-      request->yaw = 90.0;
+      request->yaw = 0.0;
 
+      std::cout << "Running req" << std::endl;
       auto result = goto_client->async_send_request(request);
+
+      sleep(5);
+      request->yaw = 1.57;
+
+      std::cout << "Running req" << std::endl;
+      result = goto_client->async_send_request(request);
+
+      std::cout << "Req returned " << std::endl;
 
     }
 
@@ -47,15 +58,15 @@ class NavTest : public rclcpp::Node{
     }
 
     void aruco_callback(const ros2_aruco_interfaces::msg::ArucoMarkers::SharedPtr msg){
-      aruco_id = msg->markers.marker_ids[0];
-      if((msg->markers.size()) > 0 && (aruco_id != aruco_id_old){ 
+      aruco_id = msg->marker_ids[0];
+      if( (msg->poses.size() > 0) && (aruco_id != aruco_id_old)){ 
 	icuas25_msgs::msg::TargetInfo target_info;
-	target_info.id = msg->markers.marker_ids[0];
+	target_info.id = msg->marker_ids[0];
 	tf2::Quaternion q;
 	tf2::Vector3 v;
 	tf2::fromMsg(agent_pose.pose.pose.orientation, q);
 	// Place aruco pose in v
-	tf2::fromMsg(msg->markers[0].pose.position, v);
+	tf2::fromMsg(msg->poses[0].position, v);
 	tf2::Vector3 rotated_v = tf2::quatRotate(q, v);
 	// Rotate the vision vector to the agent frame
 	target_info.location.x = agent_pose.pose.pose.position.x + rotated_v.x(); 
@@ -64,16 +75,14 @@ class NavTest : public rclcpp::Node{
 	target_publisher->publish(target_info);
 
 	// Call the service
-	request->goal.x = -10.0; 
-	request->goal.y = -30.0; 
+	request->goal.x = -15.0; 
+	request->goal.y = -2.0; 
 
 	auto result = goto_client->async_send_request(request);
 
 	aruco_id_old = aruco_id;
       }
     }
-
-  }
 
   private:
 
@@ -82,7 +91,7 @@ class NavTest : public rclcpp::Node{
 
     nav_msgs::msg::Odometry agent_pose;
 
-    crazyflie_interfaces::srv::Goto::Request::SharedPtr request;
+    crazyflie_interfaces::srv::GoTo::Request::SharedPtr request;
 
     //rclcpp::TimerBase::SharedPtr control_timer;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr agent_pose_subscriber;
@@ -90,7 +99,7 @@ class NavTest : public rclcpp::Node{
 
     rclcpp::Publisher<icuas25_msgs::msg::TargetInfo>::SharedPtr target_publisher;
 
-    rclcpp::Client<crazyflie_interfaces::srv::Goto>::SharedPtr goto_client;
+    rclcpp::Client<crazyflie_interfaces::srv::GoTo>::SharedPtr goto_client;
 
 };
 
